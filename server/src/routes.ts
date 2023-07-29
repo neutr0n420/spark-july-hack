41c93e6281f2e2b167561f7c16a7ed279fb6e76f
KaKashi1210 <desmondaditya@gmail.com>
2023-07-29 22:09:58 +0530

    feat: added routes that queries to db

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,21 @@
-import { Express, Request, Response } from "express";
+import { Express, Request, Response, response } from "express";
+import SQL from "./utils/connect";
+
+type temp = {
+  email: string 
+  name: string
+}
 
 function routes(app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
+
+  app.get('/', async (request , response) => {const getResult = await SQL`select * from users`;response.json(getResult);})
+
+  app.post('/api/add', async(req: Request , res: Response) => {
+  const {email , name} : temp = req.body
+    const newObj = await SQL`insert into users(name ,email) values (${name} , ${email}) RETURNING *;`
+    res.json(`User added with ID: ${newObj[0].id}`)
+  } )
 }
 
 export default routes;
--- server/src/utils/connect.ts
+++ server/src/utils/connect.ts
@@ -5,4 +5,4 @@ require('dotenv').config();
 const {DATABASE_URL} = process.env;
 const SQL = postgres(DATABASE_URL,{ssl : 'require'})
 
-module.exports = SQL
+export default SQL
