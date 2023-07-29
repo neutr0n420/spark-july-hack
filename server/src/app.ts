5176ef111284d6791dffdc4c8cdc6fb860f28b37
Atharva Pardeshi <atharva.exe@gmail.com>
2023-07-30 01:06:34 +0530

    refactor: clean up app

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,14 +2,14 @@ import express, { Request, Response, request, response } from "express";
 import config from "config";
 import routes from "./routes";
 import logger from "./utils/logger";
-import SQL from "./utils/connect"
-import RedisClient from "./utils/redis";
+
 const port = config.get<number>("port");
+
 const app = express();
+
 app.use(express.json());
-RedisClient()
+
 app.listen(port, async () => {
-  
   logger.info(`App is running at http://localhost:${port}`);
   routes(app);
 });
