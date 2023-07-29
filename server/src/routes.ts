4b5c6cdce3440a0e835f2926863e2eef246a0e6d
aryan bramhane <aryanbramhane1@gmail.com>
2023-07-30 02:01:54 +0530

    refactor: restored sazed's code (lol) and removed test key

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
 import { Express, Request, Response } from "express";
 import SQL from "./utils/connect";
-import { createClient } from "redis";
+import { CreateClient } from "./utils/redis";
+import { nanoid } from "nanoid";
 
 type temp = {
   email: string;
@@ -15,22 +16,47 @@ function routes(app: Express) {
     res.json(getResult);
   });
 
-  app.post('/api/add', async(req: Request , res: Response) => {
-  const {email , name} : temp = req.body
-    const newObj = await SQL`insert into users(name ,email) values (${name} , ${email}) RETURNING *;`
-    res.json(`User added with ID: ${newObj[0].id}`)
-  } )
-  app.get('/api/resolve/:url', async(req:Request, res:Response)=>{
-    const client = createClient()
-    await client.connect()
-    
-    await client.set('aryan', "https://google.com")
-   const originalUrl:string|null = await client.get(req.params.url)
-  if(originalUrl){
-    res.redirect(originalUrl)
-  }
-  res.status(404).send("Enter valid URL!")
-  })
+  app.post("/api/add", async (req: Request, res: Response) => {
+    const { email, name }: temp = req.body;
+    const newObj =
+      await SQL`insert into users(name ,email) values (${name} , ${email}) RETURNING *;`;
+    res.json(`User added with ID: ${newObj[0].id}`);
+  });
+  app.get("/api/:url", async (req: Request, res: Response) => {
+    const client = CreateClient();
+    await client.connect();
+
+    const originalUrl: string | null = await client.get(req.params.url);
+    if (originalUrl) {
+      res.redirect(originalUrl);
+    }
+    res.status(404).send("Enter valid URL!");
+  });
+  app.post("/api/add", async (req: Request, res: Response) => {
+    const { email, name }: temp = req.body;
+    const newObj =
+      await SQL`insert into users(name ,email) values (${name} , ${email}) RETURNING *;`;
+    res.json(`User added with ID: ${newObj[0].id}`);
+  });
+
+  // Shorten endpoint
+  app.post("/api/shorten", async (req: Request, res: Response) => {
+    const { url } = req.body;
+    if (!url) {
+      res.status(400).json({ error: "URL is required" });
+    }
+    // database instance
+    const r = CreateClient();
+    await r.connect();
+
+    // generate randoms short id
+    const id = nanoid();
+
+    // set custom id to the original url
+    await r.set(id, url);
+
+    res.status(200).json({ url, id });
+  });
 }
 
 export default routes;
