b5d7c9b45eaeaf95e2c18c82e69c636837357477
Atharva Pardeshi <atharva.exe@gmail.com>
2023-07-30 17:05:21 +0530

    fix: remove styling

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './app.tsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <div className='bg-[#0D47A1] min-h-screen flex justify-center items-center'>
-            <App />
-      </div>
-  </BrowserRouter>
-  </React.StrictMode>,
-)
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
