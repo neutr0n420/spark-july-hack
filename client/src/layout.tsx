7612474b1847abbf308bc5df84f533b1e5737663
Atharva Pardeshi <atharva.exe@gmail.com>
2023-07-30 17:46:04 +0530

    feat: change background based on current pathname

--- client/src/layout.tsx
+++ client/src/layout.tsx
@@ -1,10 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "./components/footer";
 import Nav from "./components/nav";
+import { cn } from "./lib/utils";
 
 const Layout = () => {
+  const location = useLocation();
+
   return (
-    <div className="flex min-h-screen flex-col">
+    <div
+      className={cn(
+        "flex min-h-screen flex-col",
+        location.pathname == "/" && "bg-black text-[#D6D6D6]"
+      )}
+    >
       <header className="container">
         <Nav />
       </header>
