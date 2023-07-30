6c79c91d9bedb2f49ff11339d50a693bce05ad49
Atharva Pardeshi <atharva.exe@gmail.com>
2023-07-30 19:15:44 +0530

    fix: remove unwanted component, fix form

--- client/src/pages/Forms.tsx
+++ client/src/pages/Forms.tsx
@@ -1,7 +1,7 @@
-import * as z from 'zod'
-import {zodResolver} from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-import { Button } from "@/components/ui/button"
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -10,91 +10,102 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { useState } from 'react'
-import axios from 'axios'
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import axios from "axios";
 const FormSchema = z.object({
-        email : z.string().email(),
-        password : z.string(),
-        rollnumber : z.number(),
-    })
+  email: z.string().email(),
+  password: z.string(),
+  rollnumber: z.number(),
+});
 
-const AttendanceForm:React.FC = () => {
-    const [email, setEmail] = useState('')
-    const [password , setPassword] = useState('')
-    const [rollnumber , setRollNumber] = useState('')
-    const [temp , setTemp] = useState()
+const AttendanceForm: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rollnumber, setRollNumber] = useState("");
+  const [temp, setTemp] = useState();
 
-    const form =useForm<z.infer<typeof FormSchema>>({
-        resolver:zodResolver(FormSchema) 
-    })
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
 
-    const onSubmit = async(e) => {
-        e.preventDefault()
-        const newObj:object = {email,password,rollnumber}
-        axios.post('/form',newObj).then(
-          res => 
-        )
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const newObj: object = { email, password, rollnumber };
+    axios.post("/form", newObj).then((res) => {
+      console.log(res.data);
+    });
+  };
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={onSubmit} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    required
+                    type="email"
+                    placeholder="shadcn"
+                    {...field}
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                  />
+                </FormControl>
+                <FormDescription>This is your Email</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="****"
+                    {...field}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </FormControl>
+                <FormDescription>This is your Password.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="rollnumber"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>rollnumber</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="17"
+                    {...field}
+                    value={rollnumber}
+                    onChange={(e) => setRollNumber(e.target.value)}
+                  />
+                </FormControl>
+                <FormDescription>This is your Rollnumber.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </>
+  );
+};
 
-    }
-    return (
-        <>
-         <Form {...form}>
-      <form onSubmit={onSubmit} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input required type='email' placeholder="shadcn" {...field} onChange={e => setEmail(e.target.value)} value={email}/>
-              </FormControl>
-              <FormDescription>
-                This is your Email
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input  type='password' placeholder="****" {...field} value={password} onChange={e => setPassword(e.target.value)}/>
-              </FormControl>
-              <FormDescription>
-                This is your Password.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-         <FormField
-          control={form.control}
-          name="rollnumber"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>rollnumber</FormLabel>
-              <FormControl>
-                <Input placeholder="17" {...field} value={rollnumber} onChange={e => setRollNumber(e.target.value)} />
-              </FormControl>
-              <FormDescription>
-                This is your Rollnumber.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form> 
-        </>
-    )
-}
-
-export default AttendanceForm
+export default AttendanceForm;
