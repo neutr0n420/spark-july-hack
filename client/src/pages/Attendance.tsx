dfc4dc9456a2fabd705c4d8197999d39f9bb02bf
KaKashi1210 <desmondaditya@gmail.com>
2023-07-30 18:33:28 +0530

    feat: connecting backend with frontend

--- client/src/pages/Attendance.tsx
+++ client/src/pages/Attendance.tsx
@@ -1,8 +1,11 @@
 import {QRCodeCanvas} from 'qrcode.react'
+import { useState } from 'react'
+
 const Attandance = () =>{
+const [url , setUrl ] = useState('http://localhost:5173/forms')
  return(
    <div className='w-1/2 m-auto'>
-      <QRCodeCanvas value='http:localhost:5173/forms' size={600}/>
+      <QRCodeCanvas value={url} size={600}/>
    </div>
  )
 }
